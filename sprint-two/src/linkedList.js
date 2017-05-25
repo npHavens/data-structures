be9a509f460080c661f210b24e4f3d80cca28083
Nick Havens <npahavens@gmail.com>
2017-05-25 14:54:13 -0500

    started implmentation of linked list methods

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,16 +4,53 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // console.log('Adding a node here');
+    // console.log('list was', JSON.stringify(list));
+    var node = Node(value);
+    // if(list.head === null){
+    //   list.head = node;
+    // }
+    list.head = list.tail;
+    list.tail = node;
+    // console.log('list is now', JSON.stringify(list));
   };
 
+
+
   list.removeHead = function() {
+    // console.log('list was', JSON.stringify(list));
+    // console.log('');
+    // console.log('Value of the list.head: ');
+    // console.log(list.tail);
+
+    var formerHeadValue = list.tail.value;
+    //console.log('former head value is :' + formerHeadValue);
+
+    list.head = list.tail;
+    list.tail = null;
+    // console.log('list is now: ' + JSON.stringify(list));
+
+
+    return formerHeadValue;
   };
 
   list.contains = function(target) {
+        console.log('list is now: ' + JSON.stringify(list));
+
+    for( var keys in list){
+      console.log(keys);
+      console.log(keys.value);
+      if(keys.value === target){
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
 };
+
+
 
 var Node = function(value) {
   var node = {};
@@ -26,4 +63,8 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+
+
+
  */
