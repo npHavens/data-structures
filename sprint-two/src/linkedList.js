2ae07752865ba7c4f9968229e6105944d1d9b012
Nick Havens <npahavens@gmail.com>
2017-05-27 13:00:37 -0500

    completed implementation of linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,30 +4,25 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
-    // console.log('Adding a node here');
-    // console.log('list was', JSON.stringify(list));
+    // console.log('was', JSON.stringify(list))
     var node = Node(value);
-    // if(list.head === null){
-    //   list.head = node;
-    // }
-    list.head = list.tail;
-    list.tail = node;
-    // console.log('list is now', JSON.stringify(list));
+    if(!list.head){
+      list.head = node;
+      list.tail = node;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+    }
+    // console.log('now', JSON.stringify(list));
   };
 
-
-
   list.removeHead = function() {
-    // console.log('list was', JSON.stringify(list));
-    // console.log('');
-    // console.log('Value of the list.head: ');
-    // console.log(list.tail);
 
     var formerHeadValue = list.tail.value;
-    //console.log('former head value is :' + formerHeadValue);
+    // console.log('former head value is :' + formerHeadValue);
 
-    list.head = list.tail;
-    list.tail = null;
+    list.head = list.head.next;
+    // list.tail = null;
     // console.log('list is now: ' + JSON.stringify(list));
 
 
@@ -35,13 +30,17 @@ var LinkedList = function() {
   };
 
   list.contains = function(target) {
-        console.log('list is now: ' + JSON.stringify(list));
 
-    for( var keys in list){
-      console.log(keys);
-      console.log(keys.value);
-      if(keys.value === target){
+    var obj = list.head;
+    //console.log(JSON.stringify(obj));
+    while (obj){
+      // console.log(JSON.stringify(obj));
+      // console.log('This is the obj.value: ' + obj.value);
+      if (obj.value === target) {
+        //console.log('true')
         return true;
+      } else {
+        obj = obj.next;
       }
     }
     return false;
@@ -63,7 +62,9 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
-
+//list.addToTail = O(1)
+//list.removeHead = O(1)
+//list.contains = O(n)
 
 
 
