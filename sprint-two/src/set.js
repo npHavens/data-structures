89a76ede50f49439ace6f4e5ef71a271e8c82b42
Nick Havens <npahavens@gmail.com>
2017-05-27 19:28:58 -0500

    completed implementation of Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,35 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this._storage[item]) {
+    this._storage[item] = item;
+  }
 };
 
 setPrototype.contains = function(item) {
+  for (var key in this._storage) {
+    if (this._storage[key] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+//.add = O(1)
+//.contains = O(n)
+//.remove = O(1)
+
+
  */
