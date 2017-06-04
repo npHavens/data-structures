b99abab02616617689533b8ffeb9a4abcf3a5c0e
Nick Havens <npahavens@gmail.com>
2017-06-03 19:03:10 -0500

    started implementation of hashtable.resize

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,8 +1,9 @@
 
 
-var HashTable = function() {	
+var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this.size = 0;
 };
 
 HashTable.prototype.insert = function(k, v) {
@@ -10,7 +11,7 @@ HashTable.prototype.insert = function(k, v) {
   var bucket = this._storage.get(index);
   var tuple = [k, v];
   var newBucket = [];
- 
+  this.size++;
   if (bucket) {
     newBucket = _.map(bucket, function(element, i) {
       if (bucket[i][0] === k) {
@@ -21,6 +22,7 @@ HashTable.prototype.insert = function(k, v) {
   }
   newBucket.push(tuple);
   this._storage.set(index, newBucket);
+  this.resize();
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -36,16 +38,53 @@ HashTable.prototype.retrieve = function(k) {
 };
 
 HashTable.prototype.remove = function(k) {
+  var table = this;
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
   _.each(bucket, function(element, i) {
     if (bucket[i][0] === k ) {
+      table.size--;
       bucket[i] = undefined;
     }
   });
+  this.resize();
 };
 
+HashTable.prototype.resize = function(){
+  var change = this.size / this._limit;
+  //console.log(JSON.stringify(this._storage));
+  if(change > .75){
+    this._limit*=2;
+  }
+  if(change < .25){
+    this._limit*= .5;
+  }
 
+  var table = this;
+  var duplicate = this.copy();
+
+  this._storage = LimitedArray(this._limit);
+
+  _.each(duplicate,function(tuple){
+     //console.log(JSON.stringify(tuple));
+      //if(tuple) {
+        //table.insert(tuple[0], tuple[1]);
+      //}
+  });
+  //console.log(JSON.stringify(this._storage));
+
+};
+
+HashTable.prototype.copy = function(){
+  var copy = [];
+  this._storage.each(function(bucket){
+    if(bucket){
+      copy = copy.concat(bucket);
+    }
+  });
+  console.log(JSON.stringify(copy));
+  return copy;
+};
 
 /*
  * Complexity: What is the time complexity of the above function.s?
